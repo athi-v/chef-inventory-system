14c416c8fbaffac8771039c9c5bf47a0bfac4b01
Athenkosi Vinqi <nervadevelop@gmail.com>
2023-07-14 04:00:02 +0200

    added docker

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const color = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const cors = require('cors');
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
 
 connectDB()
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
